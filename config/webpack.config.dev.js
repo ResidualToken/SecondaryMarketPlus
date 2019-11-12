419a09cfd7b102f4d87f66b888347202da780ccf
Ryan Iot <ryan.w.medlin@gmail.com>
2019-11-12 13:14:05 -0800

    Update webpack.config.dev.js

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -14,11 +14,11 @@ const paths = require('./paths');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
-const publicPath = 'http://http://ec2-18-219-153-13.us-east-2.compute.amazonaws.com:3001/';
+const publicPath = 'http://ec2-18-219-153-13.us-east-2.compute.amazonaws.com:3001/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = 'http://localhost:3001/';
+const publicUrl = 'http://ec2-18-219-153-13.us-east-2.compute.amazonaws.com:3001/';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
